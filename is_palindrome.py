e154d8548527a7f1a2ece94fa3c56dddf6b83252
Артур <46723673+Artur794@users.noreply.github.com>
2024-06-10 17:25:34 +0500

    Delete is_palindrome.py

--- is_palindrome.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(data: str) -> bool:
-    return data == data[::-1]
-
-if __name__ == "__main__":
-    data = input().strip()
-    if is_palindrome(data):
-        print("YES")
-    else:
-        print("NO")
